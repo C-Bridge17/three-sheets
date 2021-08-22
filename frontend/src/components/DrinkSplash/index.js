06e4b27bd6093b6033a2635198ae6e0f4699d7a6
Curtis Bridge <c.bridge0496@gmail.com>
2021-08-22 15:08:43 -0400

    slice on drink splash

--- frontend/src/components/DrinkSplash/index.js
+++ frontend/src/components/DrinkSplash/index.js
@@ -18,7 +18,7 @@ const DrinkSplash = ({ feed }) => {
   return (
     <div>
       <h2 className='sips-home-title'>Recent Sips</h2>
-      {arr.map(el => (
+      {arr.slice(0, 15).map(el => (
         <div className='recent-sip-div' key={el.id}>
           <img className='sip-image' src={el.Drink.imageUrl} alt={`Logo for ${el.Drink.name}`} height='50px' width="50px"></img>
           <NavLink to={`/drinks/${el.drinkId}`}><h3>{el.Drink.name}</h3></NavLink>
